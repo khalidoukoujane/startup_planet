b463342005c2242097e2cc30840e83bd1bf846a7
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-11-04 16:11:40 +0100

    added query filters

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 import express from "express"
+import {startups} from "./data.js"
 
 const PORT = 8000;
 
 const app = express();
+app.get("/api", (req, res) => {
+	let filteredData = startups;
+	const {industry, country, continent, is_seeking_funding, has_mvp} = req.query;
+	if (industry) {
+		filteredData =filteredData.filter(startup => 
+			startup.industry.toLocaleLowerCase() === industry.toLocaleLowerCase()
+		)
+	}
+	if (country) {
+		filteredData =filteredData.filter(startup => 
+			startup.country.toLocaleLowerCase() === country.toLocaleLowerCase()
+		)
+	}
+	if (continent) {
+		filteredData =filteredData.filter(startup => 
+			startup.continent.toLocaleLowerCase() === continent.toLocaleLowerCase()
+		)
+	}
+	if (is_seeking_funding) {
+		filteredData =filteredData.filter(startup => 
+			startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLocaleLowerCase())
+		)
+	}
+	if (has_mvp) {
+		filteredData =filteredData.filter(startup => 
+			startup.has_mvp === JSON.parse(has_mvp.toLocaleLowerCase())
+		)
+	}
+	res.json(filteredData);
+})
 app.listen(PORT, () => {
 	console.log(`server started on port: ${PORT}`);
-})
+}) 
