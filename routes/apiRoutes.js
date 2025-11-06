1670d534936c4670ccad569cd7f50d432e4e02f8
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-11-06 16:57:25 +0100

    routes are done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,9 @@
-import { getAllData } from "../controllers/getAllData";
-import { getDataByPathParams } from "../controllers/getDataByPathParams";
+import { getAllData } from "../controllers/getAllData.js";
+import { getDataByPathParams } from "../controllers/getDataByPathParams.js";
 import express from 'express';
+
+export const apiRouter = express.Router();
+
+apiRouter.get('/', getAllData);
+apiRouter.get('/:field/:term', getDataByPathParams);
 
