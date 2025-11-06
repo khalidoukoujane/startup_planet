4f81a2cb1756badbf3649896013db9407e18fae2
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-11-06 19:16:04 +0100

    added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { apiRouter } from "./routes/apiRoutes.js";
+import cors from 'cors';
 
 const PORT = 8000;
 
 const app = express();
+app.use(cors())
 app.use('/api', apiRouter);
 app.use((req, res) => {
 	res.status(404).json(
