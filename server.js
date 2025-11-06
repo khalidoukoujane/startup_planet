66f263e927a1a259a1a04aef58b944415d0a9925
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-11-06 16:57:11 +0100

    Modularise The Code

--- server.js
+++ server.js
@@ -1,56 +1,11 @@
 import express from "express"
-import {startups} from "./data.js"
+import { apiRouter } from "./routes/apiRoutes.js";
 
 const PORT = 8000;
 
 const app = express();
-app.get("/api", (req, res) => {
-	let filteredData = startups;
-	const {industry, country, continent, is_seeking_funding, has_mvp} = req.query;
-	if (industry) {
-		filteredData =filteredData.filter(startup => 
-			startup.industry.toLocaleLowerCase() === industry.toLocaleLowerCase()
-		)
-	}
-	if (country) {
-		filteredData =filteredData.filter(startup => 
-			startup.country.toLocaleLowerCase() === country.toLocaleLowerCase()
-		)
-	}
-	if (continent) {
-		filteredData =filteredData.filter(startup => 
-			startup.continent.toLocaleLowerCase() === continent.toLocaleLowerCase()
-		)
-	}
-	if (is_seeking_funding) {
-		filteredData =filteredData.filter(startup => 
-			startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLocaleLowerCase())
-		)
-	}
-	if (has_mvp) {
-		filteredData =filteredData.filter(startup => 
-			startup.has_mvp === JSON.parse(has_mvp.toLocaleLowerCase())
-		)
-	}
-	res.json(filteredData);
-})
-app.get("/api/:field/:term", (req, res) => {
-	const { field, term } = req.params;
-	const allowedFields = ['country', 'continent', 'industry'];
-	if (allowedFields.includes(field)) {
-		const filteredData = startups.filter(startup => 
-			startup[field].toLocaleLowerCase() === term.toLocaleLowerCase()
-		);
-		res.json(filteredData);
-	} else {
-		res.status(400);
-		res.json(
-			{
-				message: "Search field not allowed. Please use only 'country', 'continent', 'industry'"
-			});
-		return ;
-	}
-})
+app.use('/api', apiRouter);
+
 app.listen(PORT, () => {
 	console.log(`server started on port: ${PORT}`);
 }) 
